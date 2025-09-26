51f5b2c3568328dbbb9cf836cfb5e63e112b7bc3
Vedant <borodevedant4@gmail.com>
2025-09-27 02:26:46 +0530

    chore(db): add unique constraint on user email and make avatar optional

---
+++ packages/db/prisma/migrations/20250926205510_add_unique_to_user_email_and_make_avatar_optional/migration.sql
@@ -0,0 +1,11 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" ALTER COLUMN "avatar" DROP NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
