38e8ace20a9bb8d50cfcc564931420d13190e880
Vedant <borodevedant4@gmail.com>
2025-10-05 01:32:36 +0530

    feat(ws): add token verification logic via queryparam to ws-backend

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,30 @@
 import { WebSocketServer } from "ws";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { JWT_SECRET } from '@repo/backend-common/config';
+
 
 const wss = new WebSocketServer({ port : 8080});
 
-wss.on("connection",  function connection(ws){
+wss.on("connection",  function connection(ws,  req){
+
+    const url = req.url
+    if(!url){
+        ws.close();
+        return;
+    }
+
+    const queryParam = new URLSearchParams(url.split("?")[1]);
+    
+    const token = queryParam.get("token")||"" ;
+
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    if(!decoded || !(decoded as JwtPayload).userId){
+        ws.close();
+        return
+    }
+
+
     ws.on("message", function message(data) {
         ws.send("pong");
     });
