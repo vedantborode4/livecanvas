2f8acce463bfb9c3a7c97af9ad3c91557a2ba57b
Vedant <borodevedant4@gmail.com>
2025-12-07 13:11:04 +0530

    feat(CORS): configure CORS

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -3,8 +3,18 @@ import userRouter from "./routes/user.routes";
 import { authMiddleware } from "./middleware/auth.middlewares";
 import roomRouter from "./routes/room.routes";
 import { chatRouter} from "./routes/chats.routes";
+import cors from "cors";
+import "dotenv/config"
 
 const app = express();
+
+const ORIGIN = process.env.ORIGIN
+
+app.use(cors({
+    origin: ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}))
 
 app.use(express.json());
 app.use("/api/v1", userRouter)
