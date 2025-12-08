eff295c227114c7f24461f5609f74328e0313697
Vedant <borodevedant4@gmail.com>
2025-12-08 23:14:01 +0530

    feat(canvas): add feature to store existing shapes

--- apps/livecanvas-fe/draw/index.ts
+++ apps/livecanvas-fe/draw/index.ts
@@ -1,6 +1,20 @@
+type Shape = {
+    type: "rect";
+    x:number;
+    y:number;
+    height:number;
+    width:number;   
+} | {
+    type: "circle";
+    centerX: number; 
+    centerY: number; 
+    radius: number; 
+}
 export function initDraw (canvas:HTMLCanvasElement) {
 
     const ctx = canvas.getContext("2d");
+
+    let exisitingShape: Shape[] = [];
 
     if(!ctx) return;
 
@@ -19,16 +33,41 @@ export function initDraw (canvas:HTMLCanvasElement) {
 
     canvas.addEventListener("mouseup", (e) => {
         clicked=false;
-        console.log(e.clientX)
-        console.log(e.clientY)
+        const  width = (e.clientX -startX)
+        const  height = (e.clientY - startY)
+
+        exisitingShape.push({
+            type: "rect",
+            x:startX,
+            y:startY,
+            width:width,
+            height:height
+        })
     })
 
     canvas.addEventListener("mousemove", (e) => {
         if(clicked){
-            const height = e.clientX - startX;
-            const width = e.clientY -startY;
-            ctx.clearRect(0,0,canvas.width, canvas.height)
-            ctx.strokeRect(startX,startY, height, width)
+            const width = e.clientX - startX;
+            const height = e.clientY -startY;
+            
+            clearCanvas(exisitingShape, canvas,ctx);
+
+            ctx.strokeStyle = "white"
+            ctx.strokeRect(startX,startY, width,height)
         }
     })
+}
+
+function clearCanvas(exisitingShape:Shape[], canvas:HTMLCanvasElement, ctx:CanvasRenderingContext2D){
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    ctx.fillStyle= "black";
+    ctx.fillRect(0,0,canvas.width, canvas.height);
+
+    exisitingShape.map((shape) => {
+        if(shape.type === "rect"){
+            ctx.strokeStyle ==="white"
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+        }
+    })
+
 }
